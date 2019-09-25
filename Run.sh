92ece3949764f2856ce1fd56683e8762858e7428
NabilaRahman <39519361+NabilaRahman@users.noreply.github.com>
2019-09-25 11:35:28 +0100

    Executable

---
+++ Run.sh
@@ -0,0 +1,14 @@
+
+source ./Scripts/ValidateInput.sh
+functions="./Scripts/Functions.R"
+
+export inputFile  species project tx2gene gene2sym filterGenes genefilter functions rsemDir diffExpDir contrast project
+
+if [[ $validated == "true" ]]
+	then
+	./Scripts/DownloadFastQ.sh && \
+	./Scripts/Trim-BBTools.sh && \
+	./Scripts/Align-STAR.sh && \
+	./Scripts/ReadCounts-RSEM.sh && \
+	Rscript ./Scripts/SummariseGene.R
+fi
