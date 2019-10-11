de3d33bb6cbb60a711f426f11e78c0f0cf3d15ee
NabilaRahman <39519361+NabilaRahman@users.noreply.github.com>
2019-10-11 17:56:01 +0100

    Update SummariseGene.R

--- Scripts/SummariseGene.R
+++ Scripts/SummariseGene.R
@@ -5,8 +5,9 @@ functions = Sys.getenv("functions")
 project = Sys.getenv("project")
 tx2gene = Sys.getenv("tx2gene")
 contrast = strsplit(Sys.getenv("contrast"), split=",")
-filterGenes = Sys.getenv("filterGenes")
 
+filterGenes = Sys.getenv("filterGenes")
+genefilter = Sys.getenv("genefilter")
 
 source(functions)
 setwd(workDir)
@@ -16,7 +17,7 @@ require(readr)
 
 # Prepare data based on whether you want genes filtered or not ------------
 pheno<-read.table(inputSRAFile, stringsAsFactors = F)
-  colnames(pheno) <- c("accIDs","sampleNames","phenotype" )
+colnames(pheno) <- c("accIDs","sampleNames","phenotype" )
 tx2gene<-read.table(tx2gene, stringsAsFactors = F)
 
 
@@ -27,7 +28,6 @@ if (filterGenes == F ) {
   #Get counts from filtered files.
   isoforms.results <- ".isoforms.results.filt"
   filtering = "geneFilter"
-  genefilter = Sys.getenv("genefilter")
   geneFilter <- read.table(genefilter, header=F, stringsAsFactors = F, sep="\t")
   
   
@@ -82,20 +82,72 @@ contrast.matrix <- makeContrasts(contrasts=contrast, levels=design)
 
 
 #Low Count Filter -------------------------------------------------
+# Calculate Estimated Cut-Off
+# Length Adjusted Library Size
+rawLibraryLength <- gene.rsem$length
+adjustedLibraryLength <- rawLibraryLength/exp(rowMeans(log(rawLibraryLength)))
 
+# Length Adjusted Counts
+LengthAdjustedCounts <- genecounts/adjustedLibraryLength
+
+
+EffectiveLibrarySize <- calcNormFactors(LengthAdjustedCounts) * colSums( LengthAdjustedCounts )
+MeanLibSize <- mean(EffectiveLibrarySize) * 1e-6
+MedianLibSize <- median(EffectiveLibrarySize) * 1e-6
+cpmCutOff <- log2(10/MedianLibSize + 2/MeanLibSize)
+
+#Minimum CPM cutoff
+min.count = 10/MedianLibSize
+
+#Extract Length Adjusted Raw Counts
 raw<-DGEList(genecounts); 
-raw <- calcNormFactors(raw); 
+rawCPM<-cpm(raw, log = T); colnames(rawCPM)<-pheno$sampleNames
+
+saveRDS(raw, paste("geneCounts.dgeList.raw",filtering, project, "RDS", sep="."))
 
 #Remove noisy low counts and normalize
-keep <- filterByExpr(raw, design)					    #Determine which genes have stat sig large counts 
-dge <- raw[keep,,keep.lib.sizes=FALSE]    				#keeps only stat sig counts
-filteredExpr <- cpm(dge, log=T); colnames(filteredExpr)<-pheno$sampleNames
+keep <- filterByExpr(raw, design) #Determine which genes have stat sig large counts 
 
-saveRDS(dge, paste("geneCounts.filtered", project, "RDS", sep="."))
+
+filteredCounts <- raw[keep,,keep.lib.sizes=FALSE]    				#keeps only stat sig counts
+filteredCPM <- cpm(filteredCounts, log=T); colnames(filteredCPM)<-pheno$sampleNames
+
+# TMM Normalisation
+normTMMCounts <- calcNormFactors(filteredCounts, method = "TMM" ); 
+normCPM<-cpm(normTMMCounts, log=T); colnames(normCPM)<-pheno$sampleNames
+saveRDS(normTMMCounts, paste("geneCounts.dgeList.norm.",filtering, project, "RDS", sep="."))
 
 
 # Voom Normalisation ------------------------------------------------------
-v <- voom(dge, design, plot=T, save.plot = T)
+v <- voom(normTMMCounts, design, plot=F, save.plot = T)
+mv<-data.frame(lcpm=v$voom.xy$x,
+               stdv=v$voom.xy$y)
+meanLine<-v$voom.line
+
+require(ggplot2)
+mvPlot <- ggplot(mv) + 
+  geom_point(aes(x=lcpm, y=stdv), size=0.01, alpha=0.5 ) +
+  geom_line(aes(x=meanLine$x, y=meanLine$y), color="red") +
+  theme_classic() + 
+  xlab("Log2(CPM + 0.5)") +
+  ylab(v$voom.xy$ylab)
+
+mvPlotFile <- paste("mvPlot.old",filtering, project, "png", sep=".")
+if (!file.exists( mvPlotFile ) || overwrite == T  )  {
+  ggsave(
+    mvPlotFile    
+    , plot = mvPlot
+    , device = "png"
+    , height = 6
+    , width = 8
+    , scale=0.4
+    , dpi=300
+  )
+}
+
+
+
+# Differential Expression -------------------------------------------------
 fit<-lmFit(v,design)
 fit2 <- contrasts.fit(fit, contrast.matrix)
 fit2 <- eBayes(fit2)
@@ -113,9 +165,12 @@ for ( i in 1:length(contrast) ) {
 
 # Quality Plots
 ## Box Plot ----------------------------------------------------------------
-va <-v$E; colnames(va) <- pheno$sampleNames
-boxRaw <- ggBoxPlot(data = filteredExpr, title = "A")
-boxNorm <- ggBoxPlot(data = va, title = "B")
+raw.mat<- rawCPM; colnames(raw.mat)<-pheno$phenotype
+filt.mat<-filteredCPM ; colnames(filt.mat)<-pheno$phenotype
+norm.mat<-normCPM; colnames(norm.mat)<-pheno$phenotype
+
+boxRaw <- ggBoxPlot(data = filteredCPM, title = "Unprocessed")
+boxNorm <- ggBoxPlot(data = normCPM, title = "Normalised")
 
 boxNormFile <- paste("box.norm",filtering, project, "png", sep=".")
 
@@ -128,36 +183,36 @@ a<-  ggpubr::annotate_figure( a
                                                          , vjust = -0.5
                                                          , hjust = 1
                                                          , size = 13)
-                              , left=ggpubr::text_grob("Log2(gene count + 0.5)"
+                              , left=ggpubr::text_grob("Log2(CPM + 0.5)"
                                                        , vjust = 1
                                                        , size = 13
                                                        , rot = 90 ))
 
-if (!file.exists( boxNormFile ) )  {
-  ggsave(
-    paste(boxNormFile)
-    , plot = a
-    , device = "png"
-    , height = 3
-    , width = 6
-  )
-}
+ggsave(
+  paste(boxNormFile)
+  , plot = a
+  , device = "png"
+  , height = 9
+  , width = 18
+  , dpi=300
+  , scale=0.33
+)
 
 
 ## Density plot -------------------------------------------------------------------------
-unfilteredExpr <- cpm(raw, log=T); colnames(unfilteredExpr)<-pheno$sampleNames
+cutoffLine <- geom_vline(xintercept = log2(min.count), color="black", linetype = "dashed" )
 
 #Plot Density for unfiltered
-unfilteredPlot <- densityPlot(unfilteredExpr, phenoData = pheno, title="A", fill="phenotype")
-densityRaw <- densityPlot(filteredExpr, phenoData = pheno, title="B", fill="phenotype")
-densityNorm <- densityPlot(va, phenoData = pheno, title="C", fill="phenotype")
+unfilteredPlot <- densityPlot(rawCPM, phenoData = pheno, title="Raw", fill="phenotype") + cutoffLine
+densityRaw <- densityPlot(filteredCPM, phenoData = pheno, title="Low_Count_Filtered", fill="phenotype")+ cutoffLine
+densityNorm <- densityPlot(normCPM, phenoData = pheno, title="Normalised", fill="phenotype")+ cutoffLine
 
 density2<- ggpubr::ggarrange(unfilteredPlot, densityRaw, densityNorm
                              , ncol = 3
                              , common.legend = T
                              , legend="right") 
 density2<-  ggpubr::annotate_figure( density2
-                                     , bottom=ggpubr::text_grob("Log2(gene count + 0.5)"
+                                     , bottom=ggpubr::text_grob("Log2(CPM + 0.5)"
                                                                 , vjust = -0.5
                                                                 , hjust = 1
                                                                 , size = 13)
@@ -168,38 +223,33 @@ density2<-  ggpubr::annotate_figure( density2
 
 
 densityFile <- paste("density.norm.raw",filtering, project, "svg", sep=".")
-if (!file.exists( densityFile ) )  {
-  ggsave(
-    paste(densityFile)
-    , plot = density2
-    , device = "svg"
-    , height = 3
-    , width = 9
-  )
-}
+
+ggsave(
+  paste(densityFile)
+  , plot = density2
+  , device = "svg"
+  , height = 2
+  , width = 6
+)
 
 
 ## PCA ---------------------------------------------------------------------
 
 #PCA Plot
-raw.mat<-filteredExpr ; colnames(raw.mat)<-pheno$phenotype
-norm.mat<-va; colnames(va)<-pheno$phenotype
+filtPCA <- pcaPlot(data=filt.mat, title = "Unprocessed",  shape.manual = T, shape.value = c(15,16,17,18,19,20,9), shape = colnames(filt.mat), color = colnames(filt.mat))
+normPCA <- pcaPlot(data=norm.mat, title = "Normalised", shape.manual = T, shape.value = c(15,16,17,18,19,20,9),  shape = colnames(norm.mat), color = colnames(norm.mat))
 
-rawPCA <- pcaPlot(data=raw.mat, title = "A",  shape.manual = T, shape.value = c(15,16,17,18,19,20,9), shape = colnames(raw.mat), color = colnames(raw.mat))
-normPCA <- pcaPlot(data=norm.mat, title = "B", shape.manual = T, shape.value = c(15,16,17,18,19,20,9),  shape = colnames(raw.mat), color = colnames(raw.mat))
-
-pca2<- ggpubr::ggarrange(normPCA, rawPCA
+pca2<- ggpubr::ggarrange(filtPCA, normPCA
                          , ncol = 2
                          , common.legend = T
                          , legend = "right"
 ) 
 pcaFile <- paste("pca",filtering, project, "svg", sep=".")
-if (!file.exists( pcaFile ) )  {
-  ggsave(
-    paste(pcaFile)
-    , plot = pca2
-    , device = "svg"
-    , height = 3
-    , width = 6
-  )
-}
+
+ggsave(
+  paste(pcaFile)
+  , plot = pca2
+  , device = "svg"
+  , height = 3
+  , width = 6
+)
